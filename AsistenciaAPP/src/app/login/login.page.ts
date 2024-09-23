336f10c9dc0f30003b4fbbf81af41eaf423d49a8
Bastian <bastian.m6864@gmail.com>
2024-09-23 20:34:00 -0300

    Se agregan los componentes a la vista login, se modifica tambien el archivo .ts para las funcionalidades de la vista.

--- AsistenciaAPP/src/app/login/login.page.ts
+++ AsistenciaAPP/src/app/login/login.page.ts
@@ -1,15 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  templateUrl: 'login.page.html',
+  styleUrls: ['login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
-  constructor() { }
+  public alertButtons = [{
+    text: 'Restablecer contraseña',
+    handler: (data: any) => {
+      if (data && data.user) {
+        this.enviarMensaje(data.user);
+      }
+    }
+  }];
 
-  ngOnInit() {
+  public alertInputs = [
+    {
+      name: "nombreUsuario",
+      type: "text",
+      placeholder: 'Nombre de usuario',
+    }
+  ];
+
+  user: string = "";
+  pswd: string = "";
+
+  usuarioTest: string = "Usuario1";
+  claveTest: string = "MiClav3";
+
+  constructor(
+    private alertController: AlertController, 
+    private router: Router
+  ) { }
+
+  async onSubmit() {
+    if (this.user === this.usuarioTest && this.pswd === this.claveTest) {
+      const alert = await this.alertController.create({
+        header: 'Inicio de Sesión Exitoso',
+        buttons: [{
+          text: 'Ok',
+          handler: () => {
+            this.router.navigate(['/home']);
+          }
+        }]
+      });
+
+      await alert.present();
+    } else {
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Usuario o contraseña incorrectos, intente nuevamente.',
+        buttons: ['Ok']
+      });
+
+      await alert.present();
+    }
   }
 
+  async enviarMensaje(user: string) {
+    const mensaje = `Se ha enviado un mail al correo asociado al usuario: ${user}`;
+    await this.mostrarMensaje(mensaje);
+  }
+
+  async mostrarMensaje(mensaje: string) {
+    const alert = await this.alertController.create({
+      header: "Mail enviado",
+      message: mensaje,
+      buttons: ["Ok"]
+    });
+
+    await alert.present();
+  }
 }
