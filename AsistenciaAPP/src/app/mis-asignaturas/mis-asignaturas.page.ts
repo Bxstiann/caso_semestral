f4bb1ca314df321507e96c533d3f00aedf348e69
Bastian <bastian.m6864@gmail.com>
2024-09-24 15:52:27 -0300

    Se rellena la vista mis-asignaturas con sus respectivos datos.

--- AsistenciaAPP/src/app/mis-asignaturas/mis-asignaturas.page.ts
+++ AsistenciaAPP/src/app/mis-asignaturas/mis-asignaturas.page.ts
@@ -7,6 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MisAsignaturasPage implements OnInit {
 
+
+  asignaturas = [
+    { codigo: "ASY4131",
+      nombre: "Arquitectura",
+      seccion: "001D",
+    },
+    { codigo: "CSY4111",
+      nombre: "Calidad de software",
+      seccion: "001D",
+    },
+    { codigo: "MAT4140",
+      nombre: "Estadistica descriptiva",
+      seccion: "001D",
+    },
+    { codigo: "EAY4450",
+      nombre: "Etica para el trabajo",
+      seccion: "002D",
+    },
+    { codigo: "ASY4131",
+      nombre: "Arquitectura",
+      seccion: "001D",
+    },
+
+
+  ]
   constructor() { }
 
   ngOnInit() {
